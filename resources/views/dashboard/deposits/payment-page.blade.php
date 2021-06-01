4209f60bd3ebed80aa42f5869885f93bc0192d79
Ogugua Nwanze <geraldon1997@gmail.com>
2021-06-01 09:04:32 +0100

    modified the payment page

--- resources/views/dashboard/deposits/payment-page.blade.php
+++ resources/views/dashboard/deposits/payment-page.blade.php
@@ -1,40 +1,43 @@
 @extends('layouts.dashboard')
 
 @section('content')
-<div class="col-md-12 text-center">
-    <div class="card card-tasks">
-        <div class="card-header">
-            <h4 class="card-title">Deposit with cryptocurrency to {{ config('app.name') }} </h4>
-        </div>
-        <h2 style="color:green; font-weight: bold;">NB: Please use BEP-20 Networks</h2>
-        <div class="card-body col-md-8 offset-2">
-        <b style="color:red;">After Payment, please submit proof to <a href="mailto:accounts@durantfinance">accounts@durantfinance.com</a></b>
-        <br>
-            <img src="/assets/barcode/barcode.php?s=qr&d={{ $address }}" alt="" width="250">
-
+<div class="row">
+    <div class="col-md-12 text-center">
+        <div class="card card-tasks">
+            <div class="card-header">
+                <h4 class="card-title">Deposit with cryptocurrency to {{ config('app.name') }} </h4>
+            </div>
+            <h6 style="color:green; font-weight: bold;">NB: Please use BEP-20 Networks</h6>
             <div class="row">
-            <div class="col-md-10">
-                <input type="text" value="{{ $address }}" id="address" class="form-control" style="color: black;" readonly>
-            </div>
+                <div class="card-body col-md-8 offset-md-2">
+                <b style="color:red;">After Payment, please submit proof to <a href="mailto:{{ accountsEmail() }}">{{ accountsEmail() }}</a></b>
+                <br>
+                    <img src="http://barcode.hynetcapital.com/barcode.php?s=qr&d={{ $address }}" alt="" width="250">
 
-            <div class="col-md-2">
-                <button class="btn btn-primary" id="copy-address">Copy</button>
-            </div>
-            </div>
+                    <div class="row">
+                    <div class="col-md-10">
+                        <input type="text" value="{{ $address }}" id="address" class="form-control" style="color: black;" readonly>
+                    </div>
 
-            <ul style="list-style: none;">
-                <li><h4>Amount (USD): ${{ $amount }}</h4></li>
-                <li><h4>Amount ({{ $symbol }}): <i id="coin"></i></h4></li>
-            </ul>
+                    <div class="col-md-2">
+                        <button class="btn btn-primary" id="copy-address">Copy</button>
+                    </div>
+                    </div>
 
+                    <ul style="list-style: none;">
+                        <li><b>Amount (USD): ${{ $amount }}</b></li>
+                        <li><b>Amount ({{ $symbol }}): <i id="coin"></i></b></li>
+                    </ul>
+
+                </div>
+            </div>
         </div>
     </div>
 </div>
-
 <script>
     function getRate()
     {
-        let API_KEY = "0132e439712199d70b2dda6bded5661424c020025d2bf979dcc403d0887171d1";
+        let API_KEY = "88e46acaed38e8d09f960adc39d10a1f4b7eecdb2691e3d6c8bffca3d5adb1f0";
         let COIN = "{{ $symbol }}";
         let FIAT = "{{ $amount }}";
         let URL = "https://min-api.cryptocompare.com/data/price?fsym=USD&tsyms="+COIN;
@@ -48,7 +51,7 @@
     }
 
     setTimeout(() => {
-        // getRate();
+        getRate();
     }, 1000);
 
     function copyaddress() {
